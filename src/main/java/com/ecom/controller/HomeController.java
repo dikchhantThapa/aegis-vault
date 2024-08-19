88564683b08f0073085ef6338de9df3f510ee9f8
dikchhantThapa <dikchhant.thapa1@gmail.com>
2024-08-18 18:19:26 -0700

    base html files and mapping

---
+++ src/main/java/com/ecom/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.ecom.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";     // receive index html
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";     // login.html
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";     // register.html
+    }
+}
